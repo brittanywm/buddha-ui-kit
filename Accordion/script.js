401fcccfbe5272bd45fa277c1df30d53e647a332
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-12-30 15:15:49 -0500

    Added functioning accordion component

--- Accordion/script.js
+++ Accordion/script.js
@@ -1,1 +1,13 @@
+const tabs = document.getElementsByClassName('tab');
 
+for (let tab of tabs) {
+  tab.addEventListener('click', function() {
+    this.classList.toggle('active');
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
